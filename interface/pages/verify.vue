89eb4154253a82b05f11f612f9de782eb1072b49
datit309 <61546645+datit309@users.noreply.github.com>
2023-07-14 15:17:46 +0700

    Update verify.vue

--- interface/pages/verify.vue
+++ interface/pages/verify.vue
@@ -6,7 +6,7 @@ section
                 //.row.mx-0
                 //    .col-12.col-lg-3.mx-auto
                 //        img.w-100.px-2.mb-3(src='~/assets/images/logo.png')
-                h2.text-center.mb-4 {{$t("Mobagame Verify registration")}}
+                h2.text-center.mb-4 {{$t("HideURL Verify registration")}}
                 p.fs-5.text-center.text-green(v-if="isActive" )
                     | {{$t("Your account is verified. Please login to play the game.")}}
                 p.fs-5.text-center.text-red(v-else)
@@ -33,7 +33,7 @@ export default {
         const vm = this
         vm.token = vm.$route.query.token
         // console.log(vm.token)
-        vm.verifyAccount()
+        // vm.verifyAccount()
     },
     methods: {
         async verifyAccount() {
