5bd16e91817d5fcefe8edd6c0ebc897edf0ab912
datit309 <61546645+datit309@users.noreply.github.com>
2023-07-15 22:57:52 +0700

    update

--- interface/.nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from '/Users/robert/GIC/OT/short-link-new/interface/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default /* #__PURE__ */ defuFn(inlineConfig)
--- interface/.nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComputedRef, Ref } from 'vue'
-export type LayoutKey = "blank" | "master-light"
-declare module "/Users/robert/GIC/OT/short-link-new/interface/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    layout?: false | LayoutKey | Ref<LayoutKey> | ComputedRef<LayoutKey>
-  }
-}
